c24cc83b11545ac8ac61bb7ba689d7429e20fa84
sachin1296 <sachinpatel21296@gmail.com>
2019-03-01 18:49:59 +0530

    string

---
+++ string.py
@@ -0,0 +1,12 @@
+string="sachin"
+print('string')
+
+#indexing
+
+print(string[1])
+print(string[1:])  #it print string form index 1 to end
+print(string[1:3])  # it print string form 1 to index (3-1)
+print (string[-1])   #it print strig from revers side
+
+# string is a immutable means we cant change any charecter in string 
+# ex strig[1]='p' we cant write this
