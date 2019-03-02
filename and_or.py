0a333d431edae3e3934c3721ceaae0649f1d7693
sachin1296 <sachinpatel21296@gmail.com>
2019-03-03 00:23:31 +0530

    and_or in if_else

---
+++ and_or.py
@@ -0,0 +1,9 @@
+  # operator in if_else   "and","or","in"
+
+i=10
+j=20
+
+if i==10 and j==20:   # here if both condition is true then only it print and
+    print("and")
+if i==10 or j==10:    # here any one condition is true then it print or
+    print("or")
