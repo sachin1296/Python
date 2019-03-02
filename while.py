078ac565b2b9f3bebd28f0619a64d1155edec2d4
sachin1296 <sachinpatel21296@gmail.com>
2019-03-03 01:12:39 +0530

    while in python

---
+++ while.py
@@ -0,0 +1,18 @@
+# while loop in python
+i=1
+
+while i<=10:
+    print("python")  #basic code with while
+    i=i+1
+
+
+# sum of n number
+
+i=int(input("enter number"))
+total=0
+
+while i>0:
+    total=total+i
+    i=i-1
+
+print(f"total is={total}") 
