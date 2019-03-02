a9ff4f323bd3372216ae5159bbd614d96298321a
sachin1296 <sachinpatel21296@gmail.com>
2019-03-03 01:02:08 +0530

    in opertor in if_else

--- and_or.py
+++ and_or.py
@@ -7,4 +7,8 @@ if i==10 and j==20:   # here if both condition is true then only it print and
     print("and")
 if i==10 or j==10:    # here any one condition is true then it print or
     print("or")
-# 
+#  in operator in if_else
+name="sachin"
+
+if 'a' in name:
+    print("a is present in name")
